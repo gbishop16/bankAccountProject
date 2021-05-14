f754ef392ddaec56bbe2518b6e4f0a4e6609d920
JackyZhang1219 <zhangzhengxi1219@gmail.com>
2021-05-14 11:07:57 -0700

    add multithread and timer

--- bank.py
+++ bank.py
@@ -10,6 +10,29 @@ from functools import partial
 import threading
 import pandas as pd
 from time import sleep
+import timeit
 
 # define a function that goes through the file and tries to find the account with maximum money
 def check_account(file):
+
+
+
+
+
+# mutithreading version
+def func_thread(file, out):
+    out.append(check_account(file))
+x_ls =list(range(num_max))
+thread_list = []
+results = []
+for x in x_ls:
+ thread = threading.Thread(target=func_thread, args=(x, results))
+ thread_list.append(thread)
+for thread in thread_list:
+ thread.start()
+for thread in thread_list:
+ thread.join()
+
+
+ #check the multithreading autotime
+ print sec(timeit.Timer())
